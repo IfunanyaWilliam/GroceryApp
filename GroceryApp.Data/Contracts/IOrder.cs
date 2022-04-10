116263648084e35fa71919b63d291e467979561b
IfunanyaWilliam <williamifunanya@gmail.com>
2022-04-10 13:33:49 +0100

    Add interface IOrder in Grocery.Data/Contracts

---
+++ GroceryApp.Data/Contracts/IOrder.cs
@@ -0,0 +1,16 @@
+﻿using GroceryApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryApp.Data.Repositories
+{
+    public interface IOrder : IRepository<Order>
+    {
+        void Update(IOrder order);
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+        void PaymentStatus(int id, string sessionId, string paymentIntentId);
+    }
+}
--- GroceryApp.Models/Order.cs
+++ GroceryApp.Models/Order.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,49 @@ namespace GroceryApp.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        public string AppUserId { get; set; }
+
+        [ValidateNever]
+        public AppUser AppUser { get; set; }
+
+        [Required]
+        public DateTime DateOfOrder { get; set; }
+        public DateTime DateOfShipping { get; set; }
+        public double OrderTotal { get; set; }
+
+        public string? OrderStatus { get; set; }
+
+        public string? PaymentStatus { get; set; }
+
+        public string TrackingNumber { get; set; }
+
+        public string? Carrier { get; set; }
+
+        public string? SessionId { get; set; }
+
+        public string? PaymentIntentId { get; set; }
+
+        public DateTime DateOfPayment { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        public string Name { get; set; }
 
     }
 }
